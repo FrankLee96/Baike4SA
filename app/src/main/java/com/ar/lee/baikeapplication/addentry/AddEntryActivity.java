587e468268c03bbd2c0b46bffc315ca3284f78e0
FrankLee96 <530304349@qq.com>
2016-12-21 13:08:21 +0800

    Commit 12.21 添加了Volley支持，开始测试网络

--- app/src/main/java/com/ar/lee/baikeapplication/addentry/AddEntryActivity.java
+++ app/src/main/java/com/ar/lee/baikeapplication/addentry/AddEntryActivity.java
@@ -3,6 +3,7 @@ package com.ar.lee.baikeapplication.addentry;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -16,6 +17,8 @@ public class AddEntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_entry);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("新建词条");
